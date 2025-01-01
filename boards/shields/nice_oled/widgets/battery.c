69aba6c4793f5d888fd2859f08dc4edc00fbf1c5
Kurtis <kthrasher1@hotmail.co.uk>
2025-01-01 16:28:37 +0000

    removed unnessary files, edited battery .c

--- boards/shields/nice_oled/widgets/battery.c
+++ boards/shields/nice_oled/widgets/battery.c
@@ -7,7 +7,7 @@ LV_IMG_DECLARE(bolt);
 static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
   lv_draw_label_dsc_t label_right_dsc;
   init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono,
-                 LV_TEXT_ALIGN_LEFT);
+                 LV_TEXT_ALIGN_RIGHT);
   // LV_TEXT_ALIGN_RIGHT);
 
   char text[10] = {};
@@ -15,7 +15,7 @@ static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
   sprintf(text, "%i%%", state->battery);
   // sprintf(text, "%i%%", state->battery);
   // x, y, width, dsc, text
-  lv_canvas_draw_text(canvas, 0, 50, 42, &label_right_dsc, text);
+  lv_canvas_draw_text(canvas, 26, 19, 42, &label_right_dsc, text);
   // lv_canvas_draw_text(canvas, -6, 50, 42, &label_right_dsc, text);
 }
 
@@ -25,7 +25,7 @@ static void draw_charging_level(lv_obj_t *canvas,
   lv_draw_img_dsc_init(&img_dsc);
   lv_draw_label_dsc_t label_right_dsc;
   init_label_dsc(&label_right_dsc, LVGL_FOREGROUND, &pixel_operator_mono,
-                 LV_TEXT_ALIGN_LEFT);
+                 LV_TEXT_ALIGN_RIGHT );
   // LV_TEXT_ALIGN_RIGHT);
 
   char text[10] = {};
@@ -39,12 +39,10 @@ static void draw_charging_level(lv_obj_t *canvas,
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
-  /*
   lv_draw_label_dsc_t label_left_dsc;
   init_label_dsc(&label_left_dsc, LVGL_FOREGROUND, &pixel_operator_mono,
                  LV_TEXT_ALIGN_LEFT);
   lv_canvas_draw_text(canvas, 0, 19, 25, &label_left_dsc, "BAT");
-  */
 
   if (state->charging) {
     draw_charging_level(canvas, state);
